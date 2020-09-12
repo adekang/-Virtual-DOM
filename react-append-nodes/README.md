18cee9661b6e9b041c1c8e45d297e844a03b744c
frankfang <frankfang1990@gmail.com>
2020-09-12 14:20:09 +0800

    build

--- react-append-nodes/README.md
+++ react-append-nodes/README.md
@@ -1,6 +1,12 @@
 # 测试
 
-##　步骤
+##　测试步骤
 
 1. 用浏览器打开 react-append-notes/build/index.html
 2. 点击「运行」按钮，等待执行结束
+
+## build 
+
+1. 运行 yarn install
+2. 运行 yarn build
+3. 根据上面的「测试步骤」重新测试
---
+++ react-append-nodes/build/precache-manifest.70bdd83fd9cc7a31f76a007b2ed3e1dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9fbfd7ae2da171f1f15c3db82d7f7de",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1324f482cf1898b5e033",
+    "url": "./static/js/2.877a233b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.877a233b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f71733ca436f7e60894f",
+    "url": "./static/js/main.be65552c.chunk.js"
+  },
+  {
+    "revision": "af27aebdd5d67dafb986",
+    "url": "./static/js/runtime-main.8a3b5abe.js"
+  }
+]);
